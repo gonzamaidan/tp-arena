72422591bb3d5cf960bf8a1ca20a0ce63b88607c
Abdul <abdulganem@gmail.com>
2018-08-10 14:06:48 -0300

    agrego la clase tarea

--- src/main/java/model/Fixture.java
+++ src/main/java/model/Fixture.java
@@ -7,8 +7,9 @@ public class Fixture {
 	public static void initialize() {
 		
 		Estudiante gonza = new Estudiante("1465831", "Gonzalo", "Maidan", "gonzamaidan");
-		
+		Estudiante abdu = new Estudiante("1389099", "Abdul", "Ganem", "abdulganem");
 		Repository.estudiantes.agregar(gonza);
+		Repository.estudiantes.agregar(abdu);
 		
 	}
 
---
+++ src/main/java/model/Tarea.java
@@ -0,0 +1,9 @@
+package model;
+import java.util.List;
+
+public class Tarea {
+ String nombre;
+ TipoTarea tipo;
+ Estudiante estudiante;
+ List<Calificacion> notas;
+}
